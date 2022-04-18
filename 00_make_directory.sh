07605695abd4c61a9950921327ddcfa2075028fc
Rikima Mitsuhashi <rikima.mitsuhashi@nifty.com>
2022-04-18 19:17:32 +0900

    fix

--- 00_make_directory.sh
+++ 00_make_directory.sh
@@ -2,6 +2,6 @@
 
 mkdir virustotal malware malware_image dataset
 cd dataset
-mkdir 001 002 003 004 005 006 007 008 009 010 011 012 013 014 015 016 017 018
+mkdir 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18
 cd ..
 
